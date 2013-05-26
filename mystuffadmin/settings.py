f4e30357f5987e8bc7dde4f44263e8c4e94d2bc7
Jianing Yang <jianingy.yang@gmail.com>
2013-05-26 10:07:28 +0800

    list ui redesign

--- mystuffadmin/settings.py
+++ mystuffadmin/settings.py
@@ -1,5 +1,8 @@
 # Django settings for mystuffadmin project.
 
+import os
+ROOT_PATH = os.path.dirname(__file__)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -55,7 +58,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ROOT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
