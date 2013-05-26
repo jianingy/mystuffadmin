16b2d44b6cefd4812fbed383b2a8107df35b77c3
Jianing Yang <jianingy.yang@gmail.com>
2013-05-26 10:14:11 +0800

    deal with static files

--- mystuffadmin/urls.py
+++ mystuffadmin/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'domain.views.ddns.list_view', name='home'),
     # url(r'^mystuffadmin/', include('mystuffadmin.foo.urls')),
+    url(r'^static/(?P.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
